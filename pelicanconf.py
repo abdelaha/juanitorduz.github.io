c5d7be8ff4edfad4d2f21d2117ce1ab365d5ec5a
Juan Orduz <juanitorduz@hotmail.com>
2017-06-28 16:58:25 +0200

    source

---
+++ pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Juan Camilo Orduz'
+SITENAME = u'Juan Camilo Orduz'
+SITEURL = 'http://juanitorduz.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME="themes/pelican-elegant"
+
+PLUGIN_PATHS=['pelican-plugins','plugins']
+PLUGINS=['liquid_tags.notebook', 'ipynb.liquid',]
+
+STATIC=['notebooks']
+
+EXTRA_HEADER = open('_nb_header.html').read()
+
+
+
