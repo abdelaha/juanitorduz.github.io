4343b99dc4ff3c37e6c85b7a91f1ac9da2fc60d8
Juan Orduz <juanitorduz@hotmail.com>
2017-06-29 00:35:27 +0200

    m

--- pelicanconf.py
+++ pelicanconf.py
@@ -45,8 +45,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-#THEME="themes/pelican-alchemy/alchemy"
-THEME="themes/pelican-elegant"
+THEME="themes/pelican-alchemy/alchemy"
+#THEME="themes/pelican-elegant"
 
 PLUGIN_PATHS=['pelican-plugins','plugins']
 PLUGINS = ['liquid_tags.notebook', 'liquid_tags.literal']
