a413d7b127633b98a55c82dffbe2e74a18176e91
Juan Orduz <juanitorduz@hotmail.com>
2018-03-01 07:36:02 +0100

    message

--- pelicanconf.py
+++ pelicanconf.py
@@ -62,7 +62,7 @@ PLUGIN_PATHS=['pelican-plugins']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
            'liquid_tags.include_code', 'liquid_tags.notebook',
-           'liquid_tags.literal', 'render_math']
+           'liquid_tags.literal', 'render_math', 'latex']
 
 STATIC_PATHS = ['notebooks', 'images', 'pages', 'documents']
 
