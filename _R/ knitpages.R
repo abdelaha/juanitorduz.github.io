55c56512d62385e2354d1d3c48f23f8d4b211cbe
Juan Camilo Orduz <juanitorduz@juans-mbp.localdomain>
2018-09-09 11:42:08 +0200

    add circle post

--- _R/ knitpages.R
+++ _R/ knitpages.R
@@ -6,40 +6,47 @@
 # run ./knitpages.R to update all knitr files that need to be updated.
 # run this script from your base content directory
 
+setwd('/Users/juanitorduz/juanitorduz.github.io/')
+
 library(knitr)
 
 KnitPost <- function(input, outfile, figsfolder, cachefolder, base.url = '/') {
+  
   opts_knit$set(base.url = base.url)
   fig.path <- paste0(figsfolder, sub(".Rmd$", "", basename(input)), "/")
   cache.path <- file.path(cachefolder, sub(".Rmd$", "", basename(input)), "/")
-
+  
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(cache.path = cache.path)
   opts_chunk$set(fig.cap = "center")
   render_markdown()
-  knit(input, output = NULL, envir = parent.frame())
+  
+  knit(input = input, output = outfile , envir = parent.frame())
 }
 
 knit_folder <- function(infolder, outfolder, figsfolder, cachefolder, force = F) {
-  for (infile in list.files(infolder, pattern = "*.Rmd", full.names = TRUE)) {
-
+  
+  for (infile in list.files(path = infolder, pattern = "*.Rmd", full.names = TRUE)) {
+    
     print(infile)
     outfile = paste0(outfolder, "/", sub(".Rmd$", ".md", basename(infile)))
     print(outfile)
-
+    
     # knit only if the input file is the last one modified
     if (!file.exists(outfile) | file.info(infile)$mtime > file.info(outfile)$mtime) {
-        KnitPost(input = infile, 
-                 outfile = outfile, 
-                 figsfolder = figsfolder, 
-                 cachefolder = cachefolder)
+      
+      KnitPost(input = infile, 
+               outfile = outfile, 
+               figsfolder = figsfolder, 
+               cachefolder = cachefolder)
     }
   }
 }
 
-knit_folder(infolder = 'notebooks', 
-            outfolder = '', 
-            figsfolder = 'images/', 
+knit_folder(infolder = '_R/R_notebooks', 
+            outfolder = 'content', 
+            figsfolder = 'content/images/', 
             cachefolder = '_caches')
+
 
 
